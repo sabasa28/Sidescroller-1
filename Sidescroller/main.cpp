f5d5a306f9caa13c3504e2a9998192f53c855deb
Felix Godziela <jose.godziela_99@hotmail.com>
2019-10-16 09:27:18 -0300

    Started Sidescroller based on Gradius from NES

---
+++ Sidescroller/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	//Gradius logic:
+	/*
+		Space ship going left to right
+		Capability of shooting oncoming obstacles
+		Free movement
+
+	*/
+
+
+
+
+
+
+
+
+
+}
