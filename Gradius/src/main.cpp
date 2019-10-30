54c27409ca6cae6ce90a95f6b4480a0c4b6e7395
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2019-10-30 10:55:13 -0300

    Implemented bullets, started implementation of bombs, deleted obsolete code

--- Gradius/src/main.cpp
+++ Gradius/src/main.cpp
@@ -5,18 +5,24 @@
 	Free movement
 
 */
-#include <fstream>
+
 #include <iostream>
 #include <cmath>
 
 #include "raylib.h"
-#include "glfw3.h"
 
 const int screen_width = 1000;
 const int screen_height = 400;
 const int cant_meteor = 5;
-const int max_bullets = 10;
+const static int max_bullets = 50;
+const static int maxBombs = 50;
+static int chosenBullet;
 float aux;
+
+void init_meteor();
+void drawBullets();
+void shootBullet();
+void moveBullets();
 
 //Structs/Classes i prefer to do this way with {} simply because that's the easiest
 //way for me to identify them on a quick read
@@ -36,14 +42,20 @@ struct Bullet {
 	Vector2 pos;
 	Vector2 speed;
 	float radius;
-	float rotation;
-	int life_spawn;
 	bool active;
 	Color color;
-	//Texture2D texture;
 };
 
 Bullet bullet[max_bullets];
+
+struct Bomb {
+	Vector2 pos;
+	Vector2 speed;
+	float radius;
+	bool active;
+	Color color;
+};
+Bomb bomb[maxBombs];
 
 struct Meteor {
 	Rectangle rec;
@@ -53,6 +65,7 @@ struct Meteor {
 	float speed;
 	bool active; //This is when the player shoots it, it desapears for a moment
 };
+Meteor meteor[cant_meteor];
 
 enum  Game_screens {
 	MENU,
@@ -63,7 +76,7 @@ enum  Game_screens {
 Game_screens game_screen;
 
 
-Meteor meteor[cant_meteor];
+
 
 
 void init_player();
@@ -127,6 +140,7 @@ void draw()
 	}
 	EndDrawing();
 }
+
 void draw_menu()
 {
 	int posX = GetScreenWidth() / 4, posY = 10, size = 40;
@@ -146,15 +160,18 @@ void draw_menu()
 	posY += 25;
 	DrawText("Space: shoot", posX, posY, size, RAYWHITE);
 }
+
 void draw_game()
 {
 	DrawRectangleRec(player.rec, player.color);
+	drawBullets();
 	for (int i = 0; i < cant_meteor; i++)
 	{
 		if (meteor[i].active)
 			DrawRectangleRec(meteor[i].rec, meteor[i].color);
 	}
 }
+
 void draw_end()
 {
 	int posX = GetScreenWidth() / 4, posY = 10, size = 40;
@@ -168,25 +185,32 @@ void update()
 	{
 		if (player.rec.y <= 0) player.rec.y = 0;
 		if (player.rec.y >= screen_height - player.rec.height) player.rec.y = screen_height - player.rec.height;
-		if (meteor[i].active)
-			meteor[i].rec.x -= meteor[i].speed * GetFrameTime();
+		meteor[i].rec.x -= meteor[i].speed * GetFrameTime();
 		if (meteor[i].rec.x <= 0)
 		{
 			aux = screen_height - meteor[i].rec.width;
 			meteor[i].rec.x = static_cast<float>(screen_width - meteor[i].rec.width);
 			meteor[i].rec.y = static_cast<float>(GetRandomValue(0, aux));
+			meteor[i].active = true;
+		}
+		for (int j = 0; j < max_bullets; j++)
+		{
+			if (CheckCollisionCircleRec(bullet[j].pos,bullet[j].radius,meteor[i].rec)&&meteor[i].active&&bullet[j].active)
+			{
+				meteor[i].active = false;
+				bullet[j].active = false;
+			}
 		}
 
-		if (CheckCollisionRecs(player.rec, meteor[i].rec))
+		if (CheckCollisionRecs(player.rec, meteor[i].rec)&&meteor[i].active)
 		{
-			meteor[i].rec.x = static_cast<float>(screen_width - meteor[i].rec.height);
-			meteor[i].rec.y = static_cast<float>(GetRandomValue(0,screen_height));
+			meteor[i].active = false;
 			player.life--;
 			if (player.life <= 0)
 				game_screen = END;
 		}
 	}
-
+	moveBullets();
 }
 //Input
 void input()
@@ -199,9 +223,15 @@ void input()
 	{
 		player.rec.y -= player.speed * GetFrameTime();
 	}
-	if (IsKeyPressed(KEY_ENTER))
+	if (IsKeyPressed(KEY_ENTER)&&(game_screen==END||game_screen==MENU))
 	{
+		init_meteor();
+		player.life = 3;
 		game_screen = GAME;
+	}
+	if (IsKeyPressed(KEY_SPACE))
+	{
+		shootBullet();
 	}
 }
 
@@ -246,10 +276,84 @@ void init_meteor()
 	}
 }
 
+void initBomb()
+{
+	for (int i = 0; i < maxBombs; i++)
+	{
+		bomb[i].active;
+		bomb[i].color = ORANGE;
+		bomb[i].radius = 10.0f;
+		bomb[i].speed.x = 100.0f;
+		bomb[i].speed.y = 100.0f;
+	}
+}
+
+void drawBombs() //CONTINUO DESDE ACA, IMPLEMENTAR BOMBAS
+{
+	for (int i = 0; i < maxBombs; i++)
+	{
+		if (bomb[i].active)
+		{
+			DrawCircle(bomb[i].pos.x, bomb[i].pos.y, bomb[i].radius, bomb[i].color);
+		}
+	}
+}
+
+void initBullet() 
+{
+	for (int i = 0; i < max_bullets; i++)
+	{
+		bullet[i].speed.y = 0.0f;
+		bullet[i].speed.x = 200.0f;
+		bullet[i].radius = 2.5f;
+		bullet[i].active = false;
+		bullet[i].color = YELLOW;
+	}
+}
+
+void drawBullets()
+{
+	for (int i = 0; i < max_bullets; i++)
+	{
+		if (bullet[i].active)
+		{
+			DrawCircle(bullet[i].pos.x, bullet[i].pos.y, bullet[i].radius, bullet[i].color);
+		}
+	}
+}
+
+void shootBullet()
+{
+	for (int i = 0; i < max_bullets; i++)
+	{
+		if (!bullet[i].active)
+		{
+			chosenBullet = i;
+			break;
+		}
+	}
+	bullet[chosenBullet].active = true;
+	bullet[chosenBullet].pos.x = player.rec.x+player.rec.width;
+	bullet[chosenBullet].pos.y = player.rec.y+player.rec.height/2;
+}
+
+void moveBullets()
+{
+	for (int i = 0; i < max_bullets; i++)
+	{
+		bullet[i].pos.x += bullet[i].speed.x*GetFrameTime();
+		if (bullet[i].pos.x>screen_width)
+		{
+			bullet[i].active = false;
+		}
+	}
+}
+
 void init()
 {
 	game_screen = MENU;
 	InitWindow(screen_width, screen_height, "This ain't a Gradius clone, it's a ripoff");
 	init_player();
 	init_meteor();
+	initBullet();
 }
